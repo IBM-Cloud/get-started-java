f180abd23c3250eba389b5f8ebd63c7f8f558d0b
Andrew Rieder <Andrew.Rieder@ibm.com>
2018-07-31 08:39:10 -0400

    Added support for MongoDB.

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-
 # Liberty getting started application
 The Getting Started tutorial for Liberty uses this sample application to provide you with a sample workflow for working with any Liberty app on IBM Cloud or in IBM Cloud Private; you set up a development environment, deploy an app locally and on the cloud, and then integrate an IBM Cloud database service in your app.
 
-The Liberty app uses the [Cloudant Java Client](https://github.com/cloudant/java-cloudant) to add information to a database and then return information from a database to the UI.
+The Liberty app may use either the [Cloudant Java Client](https://github.com/cloudant/java-cloudant) or the [MongoDB Java Client](https://mongodb.github.io/mongo-java-driver) to add information to a database and then return information from a database to the UI.
 
 <p align="center">
   <kbd>
@@ -17,3 +16,29 @@ The starter application for IBM Cloud Private guides you through a similar proce
 ## Before you begin
 
 You'll need a [IBM Cloud account](https://console.ng.bluemix.net/registration/), [Git](https://git-scm.com/downloads), [Cloud Foundry CLI](https://github.com/cloudfoundry/cli#downloads), and [Maven](https://maven.apache.org/download.cgi) installed. If you use [IBM Cloud Private](https://www.ibm.com/cloud-computing/products/ibm-cloud-private/), you need access to the [IBM Cloud Private Cloud Foundry](https://www.ibm.com/support/knowledgecenter/en/SSBS6K_2.1.0/cloud_foundry/overview.html) environment.
+
+## Connecting to a MongoDB Database
+
+You'll need access to a MongoDB instance. The below steps outline how to do this:
+
+1. Sign in to your Bluemix Console, and select the 'Create Resource' button in the top right of the page.
+2. Type 'MongoDB' in the search field and select 'Compose for MongoDB' under the 'Data & Analytics' section.
+3. Check that the name/region/organization/space/pricing fields are accurate.
+4. Select the 'Create' button in the bottom left of the page.
+5. Return to the Bluemix dashboard and select the newly created service.
+6. In the sidebar on the left side of the page, select 'Service Credentials'
+7. Select the 'New Credential' button, and then the 'Add' button in the page that appears.
+
+To prepare the application for use in a local environment, use the below steps. Otherwise, skip the steps to move on to deploying the application to IBM Cloud:
+
+1. The new credential will be listed in the table on the same page. Select the 'View credentials' option in the 'Actions' column.
+2. Copy the contents of the 'uri' field to the 'mongo_url' field in 'src/main/resources/mongo.properties'.
+3. Copy the contents of the 'ca_certificate_base64' field to the 'mongo_ssl' field in the same file as the previous step.
+
+Before deploying the application, make sure that it builds successfully by running `mvn install liberty:run-server` from the root directory of the project (where this README document is located). If no errors are shown, run `cf push` to deploy the application. Once the deployment process completes, run `cf bind-service GetStartedJava [SERVICE_NAME]`, where [SERVICE_NAME] is the name of your MongoDB service. Finally, run `cf restage GetStartedJava`. Once this finishes, you can access the application using the URL provided in the output from the 'cf push' command from earlier.
+
+## Additional Notes on Changes
+
+The application may work with either Cloudant or MongoDB. However, if both services are available, **the application will default to MongoDB**.
+
+When deployed on IBM Cloud, this application does **not** require bound MongoDB services to have any permutation of 'mongodb' in the name. User-provided services (as created with the cf utility) are also acceptable. Finally, a TrustStore does **not** have to be created prior to building/running the application. This is generated during runtime using the MongoDB service's credentials (Though the application will accept or add to a preconfigured TrustStore if one is provided). Generated TrustStores are stored in the working directory of the application, and can be deleted when the application finishes execution.
--- src/main/java/wasdev/sample/store/CloudantVisitorStore.java
+++ src/main/java/wasdev/sample/store/CloudantVisitorStore.java
@@ -1,18 +1,18 @@
-/*******************************************************************************
- * Copyright (c) 2017 IBM Corp.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/ 
+/******************************************************************************
+ * Copyright (c) 2018 IBM Corp.                                               *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ *    http://www.apache.org/licenses/LICENSE-2.0                              *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ ******************************************************************************/
 package wasdev.sample.store;
 
 import java.io.IOException;
@@ -28,95 +28,95 @@ import com.google.gson.JsonObject;
 import wasdev.sample.Visitor;
 
 public class CloudantVisitorStore implements VisitorStore{
-	
-	private Database db = null;
-	private static final String databaseName = "mydb";
-	
-	public CloudantVisitorStore(){
-		CloudantClient cloudant = createClient();
-		if(cloudant!=null){
-		 db = cloudant.database(databaseName, true);
-		}
-	}
-	
-	public Database getDB(){
-		return db;
-	}
 
-	private static CloudantClient createClient() {
-		
-		String url;
+    private Database db = null;
+    private static final String databaseName = "mydb";
 
-		if (System.getenv("VCAP_SERVICES") != null) {
-			// When running in IBM Cloud, the VCAP_SERVICES env var will have the credentials for all bound/connected services
-			// Parse the VCAP JSON structure looking for cloudant.
-			JsonObject cloudantCredentials = VCAPHelper.getCloudCredentials("cloudant");
-			if(cloudantCredentials == null){
-				System.out.println("No cloudant database service bound to this application");
-				return null;
-			}
-			url = cloudantCredentials.get("url").getAsString();
-		} else {
-			System.out.println("Running locally. Looking for credentials in cloudant.properties");
-			url = VCAPHelper.getLocalProperties("cloudant.properties").getProperty("cloudant_url");
-			if(url == null || url.length()==0){
-				System.out.println("To use a database, set the Cloudant url in src/main/resources/cloudant.properties");
-				return null;
-			}
-		}
+    public CloudantVisitorStore(){
+        CloudantClient cloudant = createClient();
+        if(cloudant!=null){
+            db = cloudant.database(databaseName, true);
+        }
+    }
 
-		try {
-			System.out.println("Connecting to Cloudant");
-			CloudantClient client = ClientBuilder.url(new URL(url)).build();
-			return client;
-		} catch (Exception e) {
-			System.out.println("Unable to connect to database");
-			//e.printStackTrace();
-			return null;
-		}
-	}
-	
-	@Override
-	public Collection<Visitor> getAll(){
+    public Database getDB(){
+        return db;
+    }
+
+    private static CloudantClient createClient() {
+
+        String url;
+
+        if (System.getenv("VCAP_SERVICES") != null) {
+            // When running in IBM Cloud, the VCAP_SERVICES env var will have the credentials for all bound/connected services
+            // Parse the VCAP JSON structure looking for cloudant.
+            JsonObject cloudantCredentials = VCAPHelper.getCloudCredentials("cloudant");
+            if(cloudantCredentials == null){
+                System.out.println("No cloudant database service bound to this application");
+                return null;
+            }
+            url = cloudantCredentials.get("url").getAsString();
+        } else {
+            System.out.println("Running locally. Looking for credentials in cloudant.properties");
+            url = VCAPHelper.getLocalProperties("cloudant.properties").getProperty("cloudant_url");
+            if(url == null || url.length()==0){
+                System.out.println("To use a database, set the Cloudant url in src/main/resources/cloudant.properties");
+                return null;
+            }
+        }
+
+        try {
+            System.out.println("Connecting to Cloudant");
+            CloudantClient client = ClientBuilder.url(new URL(url)).build();
+            return client;
+        } catch (Exception e) {
+            System.out.println("Unable to connect to database");
+            //e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public Collection<Visitor> getAll(){
         List<Visitor> docs;
-		try {
-			docs = db.getAllDocsRequestBuilder().includeDocs(true).build().getResponse().getDocsAs(Visitor.class);
-		} catch (IOException e) {
-			return null;
-		}
+        try {
+            docs = db.getAllDocsRequestBuilder().includeDocs(true).build().getResponse().getDocsAs(Visitor.class);
+        } catch (IOException e) {
+            return null;
+        }
         return docs;
-	}
+    }
 
-	@Override
-	public Visitor get(String id) {
-		return db.find(Visitor.class, id);
-	}
+    @Override
+    public Visitor get(String id) {
+        return db.find(Visitor.class, id);
+    }
 
-	@Override
-	public Visitor persist(Visitor td) {
-		String id = db.save(td).getId();
-		return db.find(Visitor.class, id);
-	}
+    @Override
+    public Visitor persist(Visitor td) {
+        String id = db.save(td).getId();
+        return db.find(Visitor.class, id);
+    }
 
-	@Override
-	public Visitor update(String id, Visitor newVisitor) {
-		Visitor visitor = db.find(Visitor.class, id);
-		visitor.setName(newVisitor.getName());
-		db.update(visitor);
-		return db.find(Visitor.class, id);
-		
-	}
+    @Override
+    public Visitor update(String id, Visitor newVisitor) {
+        Visitor visitor = db.find(Visitor.class, id);
+        visitor.setName(newVisitor.getName());
+        db.update(visitor);
+        return db.find(Visitor.class, id);
 
-	@Override
-	public void delete(String id) {
-		Visitor visitor = db.find(Visitor.class, id);
-		db.remove(id, visitor.get_rev());
-		
-	}
+    }
 
-	@Override
-	public int count() throws Exception {
-		return getAll().size();
-	}
+    @Override
+    public void delete(String id) {
+        Visitor visitor = db.find(Visitor.class, id);
+        db.remove(id, visitor.get_rev());
+
+    }
+
+    @Override
+    public int count() throws Exception {
+        return getAll().size();
+    }
 
 }
